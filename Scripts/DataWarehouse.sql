7bea3c9fb38761d6e5100ad088b537960fabef35
RuthLechuga <ruthlechuga.1997@gmail.com>
2020-09-03 01:10:15 -0600

    llaves foraneas en compras, ventas -> sqlserver y mysql

--- Scripts/DataWarehouse.sql
+++ Scripts/DataWarehouse.sql
@@ -29,7 +29,8 @@ CREATE TABLE PIVOTE(
 );
 
 CREATE TABLE CLIENTE(
-	CodCliente varchar(15) primary key,
+	IdCliente int IDENTITY(1,1) primary key,
+	CodCliente varchar(50) NOT NULL,
 	NombreCliente varchar(150) NOT NULL,
 	TipoCliente varchar(15) NOT NULL,
 	DireccionCliente varchar(250) NOT NULL,
@@ -37,12 +38,14 @@ CREATE TABLE CLIENTE(
 );
 
 CREATE TABLE VENDEDOR(
-	CodVendedor varchar(15) primary key,
+	IdVendedor int IDENTITY(1,1) primary key,
+	CodVendedor varchar(15) NOT NULL,
 	NombreVendedor varchar(150) NOT NULL
 );
 
 CREATE TABLE PRODUCTO(
-	CodProducto varchar(15) primary key,
+	IdProducto int IDENTITY(1,1) primary key,
+	CodProducto varchar(15) NOT NULL,
 	NombreProducto varchar(150) NOT NULL,
 	GrupoProducto varchar(50) NOT NULL,
 	TipoProducto varchar(50) NOT NULL,
@@ -50,23 +53,30 @@ CREATE TABLE PRODUCTO(
 );
 
 CREATE TABLE SUCURSAL(
-	CodSucursal varchar(15) primary key,
+	IdSucursal int IDENTITY(1,1) primary key,
+	CodSucursal varchar(15) NOT NULL,
 	NombreSucursal varchar(150) NOT NULL,
-	DireccionSucursal varchar(250) NOT NULL,
+	DireccionSucursal varchar(250) NOT NULL
+);
+
+CREATE TABLE FECHA(
+	IdFecha int IDENTITY(1,1) primary key,
+	CodFecha date NOT NULL,
+	Dia int NOT NULL,
+	Mes int Not NULL,
+	Año int NOT NULL
+);
+
+CREATE TABLE REGION(
+	IdRegion int IDENTITY(1,1) primary key,
 	Region varchar(25) NOT NULL,
 	Departamento varchar(25) NOT NULL,
 	Zona int NOT NULL
 );
 
-CREATE TABLE FECHA(
-	CodFecha date primary key,
-	Dia int NOT NULL,
-	Mes int Not NULL,
-	Año int NOT NULL,
-);
-
 CREATE TABLE PROVEEDOR(
-	CodProveedor varchar(15) primary key,
+	IdProveedor int IDENTITY(1,1) primary key,
+	CodProveedor varchar(15) NOT NULL,
 	NombreProveedor varchar(150) NOT NULL,
 	DireccionProveedor varchar(250) NOT NULL,
 	NumeroProveedor int NOT NULL,
@@ -75,20 +85,22 @@ CREATE TABLE PROVEEDOR(
 
 CREATE TABLE VENTA(
 	CodVenta int IDENTITY(1,1) primary key,
-	FK_Cliente varchar(15) FOREIGN KEY REFERENCES CLIENTE(CodCliente),
-	FK_Producto varchar(15) FOREIGN KEY REFERENCES PRODUCTO(CodProducto),
-	FK_Sucursal varchar(15) FOREIGN KEY REFERENCES SUCURSAL(CodSucursal),
-	FK_Vendedor varchar(15) FOREIGN KEY REFERENCES VENDEDOR(CodVendedor),
-	FK_Fecha date FOREIGN KEY REFERENCES FECHA(CodFecha),
+	FK_Cliente int FOREIGN KEY REFERENCES CLIENTE(IdCliente),
+	FK_Producto int FOREIGN KEY REFERENCES PRODUCTO(IdProducto),
+	FK_Sucursal int FOREIGN KEY REFERENCES SUCURSAL(IdSucursal),
+	FK_Vendedor int FOREIGN KEY REFERENCES VENDEDOR(IdVendedor),
+	FK_Fecha int FOREIGN KEY REFERENCES FECHA(IdFecha),
+	FK_Region int FOREIGN KEY REFERENCES REGION(IdRegion),
 	Unidades int NOT NULL
 );
 
 CREATE TABLE COMPRA(
 	CodCompra int IDENTITY(1,1) primary key,
-	FK_Proveedor varchar(15) FOREIGN KEY REFERENCES PROVEEDOR(CodProveedor),
-	FK_Producto varchar(15) FOREIGN KEY REFERENCES PRODUCTO(CodProducto),
-	FK_Sucursal varchar(15) FOREIGN KEY REFERENCES SUCURSAL(CodSucursal),
-	FK_Fecha date FOREIGN KEY REFERENCES FECHA(CodFecha),
+	FK_Proveedor int FOREIGN KEY REFERENCES PROVEEDOR(IdProveedor),
+	FK_Producto int FOREIGN KEY REFERENCES PRODUCTO(IdProducto),
+	FK_Sucursal int FOREIGN KEY REFERENCES SUCURSAL(IdSucursal),
+	FK_Fecha int FOREIGN KEY REFERENCES FECHA(IdFecha),
+	FK_Region int FOREIGN KEY REFERENCES REGION(IdRegion),
 	Unidades int NOT NULL
 );
 
@@ -99,6 +111,7 @@ SELECT * FROM SUCURSAL;
 SELECT * FROM CLIENTE;
 SELECT * FROM VENDEDOR;
 SELECT * FROM FECHA;
+SELECT * FROM REGION;
 SELECT * FROM VENTA;
 SELECT * FROM COMPRA;
 
@@ -110,3 +123,16 @@ DROP TABLE SUCURSAL;
 DROP TABLE CLIENTE;
 DROP TABLE VENDEDOR;
 DROP TABLE FECHA;
+DROP TABLE REGION;
+
+DELETE FROM PIVOTE;
+DELETE FROM VENTA;
+DELETE FROM COMPRA;
+DELETE FROM PRODUCTO;
+DELETE FROM PROVEEDOR;
+DELETE FROM SUCURSAL;
+DELETE FROM CLIENTE;
+DELETE FROM VENDEDOR;
+DELETE FROM FECHA;
+DELETE FROM REGION;
+
